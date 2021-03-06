70e1bb4cfb168623fb2c5e8e65ea842e4ecc7469
m.holubczat <m.holubczat@gmail.com>
2021-03-06 15:36:20 +0100

    initialize spring boot project, closes #1

---
+++ src/main/java/me/mholubczat/chucknorrisjokes/ChuckNorrisJokesApplication.java
@@ -0,0 +1,13 @@
+package me.mholubczat.chucknorrisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+    }
+
+}
